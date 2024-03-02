fe28e61ecb4ace25097f5000873d9c02f9c69004
Jakob Ecker <jakob.ecker42@gmail.com>
2024-03-02 16:10:59 +0100

    Add save keyboard shortcut

--- desktop-app/src/main/kotlin/info/marozzo/hematoma/Main.kt
+++ desktop-app/src/main/kotlin/info/marozzo/hematoma/Main.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.primarySurface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.awaitApplication
 import arrow.continuations.SuspendApp
@@ -23,6 +24,7 @@ import info.marozzo.hematoma.components.CompetitorsList
 import info.marozzo.hematoma.components.Header
 import info.marozzo.hematoma.contract.AddCompetitor
 import info.marozzo.hematoma.contract.EventState
+import info.marozzo.hematoma.contract.Save
 import info.marozzo.hematoma.domain.RegistrationNumber
 
 private val logger = FluentLogger.forEnclosingClass()!!
@@ -34,7 +36,14 @@ fun main(args: Array<String>) = SuspendApp {
         val vm = remember(coroutineScope) { EventViewModel(coroutineScope) }
         val state by vm.observeStates().collectAsState()
 
-        Window(onCloseRequest = this::exitApplication, title = "HEMAtoma") {
+        Window(onCloseRequest = this::exitApplication, title = "HEMAtoma", onPreviewKeyEvent = {
+            if (it.isCtrlPressed && it.key == Key.S && it.type == KeyEventType.KeyDown) {
+                vm.trySend(Save)
+                true
+            } else {
+                false
+            }
+        }) {
             App(state, vm::trySend)
         }
     }
@@ -44,7 +53,11 @@ fun main(args: Array<String>) = SuspendApp {
 @Composable
 @Suppress("ModifierMissing") // Is the top-level composable and has no use for modifier
 fun App(state: EventState, accept: AcceptFun) = MaterialTheme {
-    val nextReg = remember(state.event.competitors) { RegistrationNumber(state.event.competitors.size.inc().toString()).getOrNull() ?: error("Can't happen") }
+    val nextReg = remember(state.event.competitors) {
+        RegistrationNumber(
+            state.event.competitors.size.inc().toString()
+        ).getOrNull() ?: error("Can't happen")
+    }
     Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Header(state, accept, modifier = Modifier.background(MaterialTheme.colors.primarySurface))
         CompetitorNameInput(number = nextReg, onSubmit = { accept(AddCompetitor(nextReg, it)) })
