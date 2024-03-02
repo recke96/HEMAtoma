653810c87e8fe58566efb9f797eef5954893ebec
Jakob Ecker <jakob.ecker42@gmail.com>
2024-03-02 16:51:56 +0100

    Add scrollbar

--- desktop-app/src/main/kotlin/info/marozzo/hematoma/components/Competitors.kt
+++ desktop-app/src/main/kotlin/info/marozzo/hematoma/components/Competitors.kt
@@ -6,10 +6,15 @@
 
 package info.marozzo.hematoma.components
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ExperimentalMaterialApi
@@ -20,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import info.marozzo.hematoma.AcceptFun
@@ -36,18 +42,27 @@ fun CompetitorSection(competitors: Competitors, accept: AcceptFun, modifier: Mod
             competitors.size.inc().toString()
         ).getOrNull() ?: error("Can't happen")
     }
-    CompetitorsList(competitors, modifier = Modifier.fillMaxWidth())
     CompetitorNameInput(
         number = nextReg,
         onSubmit = { accept(AddCompetitor(nextReg, it)) },
         modifier = Modifier.fillMaxWidth()
     )
+    CompetitorsList(competitors, modifier = Modifier.fillMaxWidth())
 }
 
 @Composable
-fun CompetitorsList(competitors: Competitors, modifier: Modifier = Modifier) = LazyColumn(modifier) {
-    items(competitors, key = { it.id }) {
-        CompetitorListItem(it, modifier = Modifier.fillMaxWidth())
+fun CompetitorsList(competitors: Competitors, modifier: Modifier = Modifier) {
+    val state = rememberLazyListState()
+    Box(modifier) {
+        LazyColumn(state = state) {
+            items(competitors, key = { it.id }) {
+                CompetitorListItem(it, modifier = Modifier.fillMaxWidth())
+            }
+        }
+        VerticalScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(state)
+        )
     }
 }
 
