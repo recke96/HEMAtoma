5f072e46663c47f7b959de7a91ab4ad3666c9335
Jakob Ecker <jakob.ecker42@gmail.com>
2024-03-03 17:07:20 +0100

    Implement scoring / result logic

--- domain/src/main/kotlin/info/marozzo/hematoma/domain/Combat.kt
+++ domain/src/main/kotlin/info/marozzo/hematoma/domain/Combat.kt
@@ -6,7 +6,14 @@
 
 package info.marozzo.hematoma.domain
 
+import arrow.core.getOrElse
+import arrow.core.nel
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import arrow.core.toOption
 import arrow.optics.optics
+import info.marozzo.hematoma.domain.errors.Validated
+import info.marozzo.hematoma.domain.errors.ValidationError
 import info.marozzo.hematoma.serializers.PersistentListSerializer
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
@@ -15,6 +22,11 @@ import kotlinx.serialization.Serializable
 @JvmInline
 @Serializable
 value class Score(private val value: Int) : Comparable<Score> {
+
+    companion object {
+        val zero = Score(0)
+        val seven = Score(7)
+    }
 
     operator fun plus(other: Score) = Score(value + other.value)
     override fun compareTo(other: Score): Int = value.compareTo(other.value)
@@ -25,7 +37,11 @@ value class Score(private val value: Int) : Comparable<Score> {
 @Serializable
 value class Hits(private val value: UInt) : Comparable<Hits> {
 
-    operator fun inc() = Hits(value.inc())
+    companion object {
+        val three = Hits(3U)
+    }
+
+    operator fun plus(other: Hits) = Hits(value + other.value)
     override fun compareTo(other: Hits): Int = value.compareTo(other.value)
 }
 
@@ -41,15 +57,9 @@ data class Combat(
 ) {
     companion object
 
-    val winner: CompetitorId? = when {
-        scoreA > scoreB -> a
-        scoreA < scoreB -> b
-        else -> null
-    }
-    val loser: CompetitorId? = when {
-        scoreA > scoreB -> a
-        scoreA < scoreB -> b
-        else -> null
+    fun getResults(): Pair<Result, Result> = when {
+        doubleHits >= Hits.three -> Result.doubleHitResult(a) to Result.doubleHitResult(b)
+        else -> Result(a, Matches.one, scoreA, scoreB, doubleHits) to Result(b, Matches.one, scoreB, scoreA, doubleHits)
     }
 }
 
@@ -65,5 +75,63 @@ value class Combats private constructor(
     }
 
     fun add(combat: Combat): Combats = Combats(combats.add(combat))
+
+    fun results() = combats
+        .map(Combat::getResults)
+        .flatMap(Pair<Result, Result>::toList)
+        .groupingBy(Result::competitor)
+        .aggregate<Result, CompetitorId, Result> { _, total, result, _ ->
+            total.toOption().flatMap {
+                it.plus(result).getOrNone()
+            }.getOrElse { result }
+        }
 }
 
+@JvmInline
+@Serializable
+value class Matches(val value: UInt) : Comparable<Matches> {
+
+    companion object {
+        val none = Matches(0U)
+        val one = Matches(1U)
+    }
+
+    operator fun plus(other: Matches) = Matches(value + other.value)
+    override fun compareTo(other: Matches): Int = value.compareTo(other.value)
+}
+
+@optics
+@Serializable
+data class Result(
+    val competitor: CompetitorId,
+    val matches: Matches = Matches.none,
+    val scored: Score = Score(0),
+    val conceded: Score = Score(0),
+    val doubleHits: Hits = Hits(0U)
+) {
+    companion object {
+        fun doubleHitResult(competitor: CompetitorId) = Result(
+            competitor,
+            Matches.one,
+            Score.zero,
+            Score.seven,
+            Hits.three
+        )
+    }
+
+    operator fun plus(other: Result): Validated<Result> = either {
+        ensure(competitor == other.competitor) {
+            ValidationError(
+                "Can't combine results of different competitors: $competitor, ${other.competitor}",
+                "other"
+            ).nel()
+        }
+        Result(
+            competitor,
+            matches + other.matches,
+            scored + other.scored,
+            conceded + other.conceded,
+            doubleHits + other.doubleHits
+        )
+    }
+}
